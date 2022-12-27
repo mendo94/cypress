dc48e26338b95a7717278dcc03fede4751792f4e
Rina Mendoza <rinamendozaa@gmail.com>
2022-12-27 17:22:58 -0500

    using force true to override disabled elements

--- cypress/e2e/1-getting-started/myFirstTest.cy.js
+++ cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -1,31 +1,44 @@
 const navbarText = Cypress.env('navbarText')
 
 context('My first test', () => {
-  beforeEach(() => {
+//  beforeEach(() => {
+//    cy.visit('/')
+//  })
+  before(() => {
+    cy.request('https://api.spacexdata.com/v3/missions').its('body').should('have.length', 10)
+  })
+
+  it('visits the homepage', () => {
     cy.visit('/')
   })
 
-  it('links to the actions page correctly', () => {
-    cy.visit('/')
-    cy.findAllByText('Actions').first().click({force: true})
-    cy.url().should('include', 'commands/actions')
+  it('should have an h1 on the page', () => {
+   cy.visit('/')
+    cy.get('h1').should('exist')
   })
 
-  it('lets you type in an input field', () => {
-    cy.visit('/commands/actions')
-    cy.findByPlaceholderText("Email").type("Test").should('have.value', 'Test')
-  })
 
-  it('lets you clear an input field', () => {
-    cy.visit('/commands/actions')
-    cy.findByLabelText('Describe:').type('Test description').should('have.value', 'Test description')
-    .clear().should('have.value', '')
-  })
-
-  it('lets you check a checkbox', () => {
-    cy.visit('/commands/actions')
-    cy.get('.action-checkboxes [type="checkbox"]').eq(1).check({force: true}).should('be.checked')
-  })
+//  it('links to the actions page correctly', () => {
+//    cy.visit('/')
+//    cy.findAllByText('Actions').first().click({force: true})
+//    cy.url().should('include', 'commands/actions')
+//  })
+//
+//  it('lets you type in an input field', () => {
+//    cy.visit('/commands/actions')
+//    cy.findByPlaceholderText("Email").type("Test").should('have.value', 'Test')
+//  })
+//
+//  it('lets you clear an input field', () => {
+//    cy.visit('/commands/actions')
+//    cy.findByLabelText('Describe:').type('Test description').should('have.value', 'Test description')
+//    .clear().should('have.value', '')
+//  })
+//
+//  it('lets you check a checkbox', () => {
+//    cy.visit('/commands/actions')
+//    cy.get('.action-checkboxes [type="checkbox"]').eq(1).check({force: true}).should('be.checked')
+//  })
 //  it('shows an active class for the current page', () => {
 //    cy.visit('/commands/actions')
 //    cy.get('.dropdown-menu').find('li').eq(2).should('have.class', 'active')
