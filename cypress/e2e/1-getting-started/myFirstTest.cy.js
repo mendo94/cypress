ea63b9ff2a4f9ea0bc3c7e9709e540692c7e1f61
Rina Mendoza <rinamendozaa@gmail.com>
2022-12-27 16:37:33 -0500

    using force true to override disabled elements

--- cypress/e2e/1-getting-started/myFirstTest.cy.js
+++ cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -7,8 +7,24 @@ context('My first test', () => {
 
   it('links to the actions page correctly', () => {
     cy.visit('/')
-    cy.findAllByText('Actions').last().click()
+    cy.findAllByText('Actions').first().click({force: true})
     cy.url().should('include', 'commands/actions')
+  })
+
+  it('lets you type in an input field', () => {
+    cy.visit('/commands/actions')
+    cy.findByPlaceholderText("Email").type("Test").should('have.value', 'Test')
+  })
+
+  it('lets you clear an input field', () => {
+    cy.visit('/commands/actions')
+    cy.findByLabelText('Describe:').type('Test description').should('have.value', 'Test description')
+    .clear().should('have.value', '')
+  })
+
+  it('lets you check a checkbox', () => {
+    cy.visit('/commands/actions')
+    cy.get('.action-checkboxes [type="checkbox"]').eq(1).check({force: true}).should('be.checked')
   })
 //  it('shows an active class for the current page', () => {
 //    cy.visit('/commands/actions')
